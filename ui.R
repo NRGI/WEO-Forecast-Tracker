72746a7281c3c0ccdcc823d4726cc3ef74b4868d
T. Morrison <thomas.r.morrison@gmail.com>
2018-06-07 13:57:50 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,12 +3,6 @@ library(shiny); library(data.table); library(ggplot2); library(plotly); library(
 
 
 shinyUI(fluidPage(
-  tags$head(tags$script("(function(w,d,s,l,i){w[l]=w[l]||[];
-                        w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-                        var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TK6BKNL');"
-                        )
-  ),
   shinyjs::useShinyjs(),
   ###########################################################################################
   inlineCSS(
@@ -30,30 +24,7 @@ shinyUI(fluidPage(
     ".shiny-output-error { visibility: hidden; }",
     ".shiny-output-error:before { visibility: hidden; }"
   ),
-  tags$body(
-    tags$noscript(
-      HTML(
-        '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TK6BKNL" height="0" width="0" style="display:none;visibility:hidden"></iframe>'
-        )
-      )
-    ),
-  tags$head(
-    tags$script(
-      async="", 
-      src=paste0(
-        "https://www.googletagmanager.com/gtag/js?id=",
-        ga_path
-      )
-    )
-  ),
-  tags$head(
-    tags$script(
-      paste0(
-        'window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "',
-        ga_path,'");              '
-      )
-    )
-  ),
+  
   ###########################################################################################
   
   fluidRow(column(width=1, a(img(src="nrgi_logo.png", id='logo', height="66px"),target="_blank", href="https://resourcegovernance.org/"), style="padding-top:15px; padding-left:50px;"),
